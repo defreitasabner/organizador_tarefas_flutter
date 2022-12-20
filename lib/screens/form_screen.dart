54962b2fe3f112a32857b4a9cd696e9e0337c5c2
Abner Freitas <defreitasabner@gmail.com>
2022-12-20 11:47:39 -0300

    Extraindno inputs para um novo componente chamado FormInputField().

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -28,22 +28,46 @@ class _FormScreenState extends State<FormScreen> {
           ),
           child: Column(
             children: [ 
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  controller: nameController,
-                  textAlign: TextAlign.center,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    fillColor: Colors.white70,
-                    filled: true,
-                    labelText: 'Nome da Tarefa',
-                    hintText: 'Digite o nome da nova tarefa'
-                  ),
-                ),
+              FormInputField(
+                inputLabel: 'Nome da Tarefa',
+                inputController: nameController,
+                inputHint: 'Digite o nome da nova tarefa',
               )
             ],
               )
+        ),
+      ),
+    );
+  }
+}
+
+class FormInputField extends StatelessWidget {
+  
+  final String inputLabel;
+  final TextEditingController inputController;
+  final String inputHint;
+  
+  const FormInputField({
+    Key? key,
+    required this.inputLabel,
+    required this.inputController,
+    required this.inputHint
+  }) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextFormField(
+        controller: inputController,
+        textAlign: TextAlign.center,
+        decoration: InputDecoration(
+          border: const OutlineInputBorder(),
+          fillColor: Colors.white70,
+          filled: true,
+          labelText: inputLabel,
+          hintText: inputHint
         ),
       ),
     );
